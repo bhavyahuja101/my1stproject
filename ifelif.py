2eea1ca2c96865bac6dcad003f6158f0d3f4a6f4
Bhavya <poojaahuja2261983@gmail.com>
2026-04-21 18:16:37 +0530

    formatting corrected

--- ifelif.py
+++ ifelif.py
@@ -2,4 +2,12 @@ a =  int(input("enter no. 1: "))
 b =  int(input("enter no. 2: "))
 c =  int(input("enter no. 3: "))
 d =  int(input("enter no. 4: "))
-if()
+if(a>b and a>c and a>d):
+    print("a is greatest")
+elif(b>a and b>c and b>d):
+    print("b is greatest")
+elif(c>a and c>b and c>d):
+    print("c is greatest")
+elif(d>a and d>b and d>c):
+    print("d is greatest")
+else:    print("all are equal") 
