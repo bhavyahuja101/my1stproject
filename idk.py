9c9f26fc7c534f4f97457f7360738b70525ff1f4
Bhavya <poojaahuja2261983@gmail.com>
2026-04-21 18:37:23 +0530

    .

--- idk.py
+++ idk.py
@@ -10,8 +10,15 @@ else:
     print("This is not a spam comment.")
 
 list = ["mohan" , "sohan" , "rohan"]    
-myname = input("enter your name")
+myname = input("enter your name ")
 if myname in list :
     print("you are selected")
 else:
     print("you are not in list sadddd")    
+
+username = input("Enter username: ")
+
+if(len(username)<10):
+    print("Your username contains less than 10 characters")
+else:
+    print("Your username contains more than or equal to 10 characters")
